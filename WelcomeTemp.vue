8d44d1c886cf19fccccee33001d39ce4ce02ae00
industryart <contact@industryart.design>
2022-05-16 16:13:27 -0400

    1 file modified, 1 deleted

--- WelcomeTemp.vue
+++
@@ -1,5 +0,0 @@
-<template>
-<div>
-  <WelcomeTemp />
-  </div>
-</template>
--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!--<NuxtWelcome />-->
-    <WelcomeTemp />
+    <NuxtWelcome />
   </div>
 </template>
