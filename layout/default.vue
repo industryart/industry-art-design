b77687d790e8c221618b76e9b3297a54303832a1
industryart <contact@industryart.design>
2022-07-22 02:11:45 -0400

    1 file modified

--- layout/default.vue
+++ layout/default.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="mainLayout">
-  Layout
     <section class="mainContainer">
      <slot />
     </section>
   </div>
   </template>
 
-  <style scoped></style>
+  <style scoped>
+    .mainLayout {
+      background-color:#000;
+    }
+  </style>
